f3d49cd8307be50dfbfea749f77e22e89b59f52c
NoL1eZ <nol1ez@outlook.com>
2024-06-30 22:38:53 +0300

    new commit 1

--- lession 3/HW_5.py
+++ lession 3/HW_5.py
@@ -1,14 +1,38 @@
-import re
 import json
+import pprint
+import csv
+import re
 
-email_pattern = re.compile(r'\b[a-zA-Z0-9._%+-]+@[0-9a-zA-Z.-_]+\.[a-zA-Z]+\b')
-inn_org_pattern = re.compile(r'\b\d{10}\b')
-inn_person_pattern = re.compile(r'\b\d{12}\b')
 
-def task_2(file):
+def traders:
+    with open('traders.txt', 'r') as file:
+        traders_inn = file.read()
+    with open('traders.json', 'r') as file:
+        data = json.load(file)
+
+    traders = []
+
+    for trader in data:
+        if trader['inn'] in traders_inn:
+            traders.append(trader)
+
+    with open('traders.csv', 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['INN', 'OGRN', 'ADDRESS'])
+        for i in traders:
+            writer.writerow([i['inn'], i['ogrn'], i['address']])
+
+
+email_exprission = re.compile(r'\b[a-zA-Z0-9._%+-]+@[0-9a-zA-Z.-_]+\.[a-zA-Z]+\b')
+inn_org_exprission = re.compile(r'\b\d{10}\b')
+inn_person_exprission = re.compile(r'\b\d{12}\b')
+
+
+def email_from_efrsb(file):
     with open(file, "r") as f:
         message = json.load(f)
     results = {}
+
     for i in message:
         emails = re.findall(email_pattern, i['msg_text'])
         for email in emails:
@@ -33,4 +57,5 @@ def task_2(file):
         json.dump(results, f)
     print("stop")
 
-task_2("1000_efrsb_messages.json")
+
+email_from_efrsb("1000_efrsb_messages.json")
